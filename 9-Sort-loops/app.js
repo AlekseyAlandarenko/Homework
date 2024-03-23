dbf28e424624a3954e6f4403b745a8efee9ef62a
AlekseyAlandarenko <AlekseyAlandarenko@gmail.com>
2024-03-23 12:00:01 +0600

    Исправил

--- 9-Sort-loops/app.js
+++ 9-Sort-loops/app.js
@@ -10,30 +10,31 @@ function sourceArray() {
         if (Boolean(Number(p)) === true || Number(p) === 0) input.push(Number(p));
         else alert('Вы ввели не число!'), --i;
     }
-    do {
-        sortOrder = prompt('Введите порядок сортировки: по убыванию(min) или по возрастанию(max).');
-        if (!validChoices.hasOwnProperty(sortOrder)) alert('Нет такого параметра сортировки!');
-    } while (!validChoices.hasOwnProperty(sortOrder));
     return input;
 }
 
 function sortingAlgorithm(input) {
+    do {
+        sortOrder = prompt('Введите порядок сортировки: по убыванию(min) или по возрастанию(max).');
+        if (!validChoices.hasOwnProperty(sortOrder)) alert('Нет такого параметра сортировки!');
+    } while (!validChoices.hasOwnProperty(sortOrder));
     switch (true) {
         case sortOrder === 'min':
             while (input.length) {
                 output.push(input.splice(input.indexOf(Math.max(...input)), 1));
             }
-            return output;
         case sortOrder === 'max':
             while (input.length) {
                 output.push(input.splice(input.indexOf(Math.min(...input)), 1));
             }
-            return output;
     }
+    console.log(output, sortOrder);
+    return [output, sortOrder];
 }
 
-alert(`Введенный массив: ${sourceArray()} 
-Массив отсортированный в порядке ${sortOrder === 'min' ? 'убывания' : 'возрастания'}: ${sortingAlgorithm(input)}`);
+alert(`Введенный массив: ${sourceArray()}`);
+sortingAlgorithm(input);
+alert(`Массив отсортированный в порядке ${sortOrder === 'min' ? 'убывания' : 'возрастания'}: ${output}`);
 
 
 
