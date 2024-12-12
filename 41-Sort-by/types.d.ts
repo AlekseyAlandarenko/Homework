5eb689e0ba7f2877b34b5c4b25693638e96ad14c
aleksey alandarenko <alekseyalandarenko@gmail.com>
2024-12-12 12:00:01 +0500

    Исправил

--- 41-Sort-by/types.d.ts
+++ 41-Sort-by/types.d.ts
@@ -1,5 +1,5 @@
 declare module 'sort-by' {
-    type SortKey<T> = keyof T | ((property: keyof T, value: T[keyof T]) => any);
+    type SortKey<T> = keyof T | (<K extends keyof T>(property: K, value: T[K]) => unknown);
 
     export function sortBy<T>(...args: SortKey<T>[]): (a: T, b: T) => number;
 }
